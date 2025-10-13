16922ec5fb2a49a620e5d704bcf88e626916bf6b
Esdras0106 <esdras.miranda@ba.estudante.senai.br>
2025-10-13 17:03:18 -0300

    Add files via upload

---
+++ FrontEnd/FrontEnd/src/Pages/HomePage/index.jsx
@@ -0,0 +1,16 @@
+import '.style.css'
+
+function HomePage(){
+    return(
+
+        <div className='home-container'>
+        <h1>Bem vindo ao sistema De Clientes</h1>
+        <p>Gerencie seus Clientes de forma eficiente e prática</p>
+        <p>Utilize o menu para navegar entre a opções disponíveis.</p>
+
+    </div>
+    );
+
+}
+
+export default HomePage;
