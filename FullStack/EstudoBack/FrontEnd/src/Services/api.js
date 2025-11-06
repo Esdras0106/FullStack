afc7c425ae9a9c0c9b3f1989e39ca2a102221452
Esdras0106 <esdras.miranda@ba.estudante.senai.br>
2025-11-06 19:08:00 +0000

    salvar

--- FullStack/EstudoBack/FrontEnd/src/Services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-// Confirguração de instância do axios com a URL base da API.
-const api = axios.create({
-    baseURL: 'http://localhost:3000',
-});
-
-export default api;
